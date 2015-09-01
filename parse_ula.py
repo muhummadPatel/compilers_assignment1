6b84688ef3aa3c2c1c377624d6198ac9b71e771e
Muhummad Patel <muhummad.patel@gmail.com>
2015-09-01 19:34:05 +0200

    Separate rules into different functions and start building up the AST(abstract syntax tree).

--- parse_ula.py
+++ parse_ula.py
@@ -5,27 +5,53 @@ import sys
 lex_tokens = lex_ula.tokens
 tokens = [t for t in lex_ula.tokens if t not in lex_ula.non_code]
 
-def p_program(p):
-	'''program	: statement
-				| program statement'''
+def p_program_terminal(p):
+	'''program	: statement'''
+	p[0] = ("Program", p[1])
 
-def p_statement(p):
+def p_program_recursive(p):
+	'''program	: program statement'''
+	p[0] = ("d", p[1], p[2])
+
+def p_statement_assignment(p):
 	'''statement	: ID EQUALS expression'''
+	p[0] = ("AssignStatement", p[1], p[3])
 
-def p_expression(p):
-	'''expression	: expression AT term
-					| expression DOLLAR term
-					| term'''
+def p_expression_add(p):
+	'''expression	: expression AT term'''
+	p[0] = ("AddExpression", p[1], p[3])
+
+def p_expression_sub(p):
+	'''expression	: expression DOLLAR term'''
+	p[0] = ("SubExpression", p[1], p[3])
+
+def p_expression_term(p):
+	'''expression	: term'''
+	p[0] = ("d", p[1])
+
+def p_term_mul(p):
+	'''term	: term HASH factor'''
+	p[0] = ("MulExpression", p[1], p[2])
+
+def p_term_div(p):
+	'''term	: term AMPERSAND factor'''
+	p[0] = ("DivExpression", p[1], p[2])
 
 def p_term(p):
-	'''term	: term HASH factor
-			| term AMPERSAND factor
-			| factor'''
+	'''term	: factor'''
+	p[0] = ("d", p[1])
 
-def p_factor(p):
-	'''factor	: LEFT_PARENTHESIS expression RIGHT_PARENTHESIS
-				| FLOAT_LITERAL
-				| ID'''
+def p_factor_brackets(p):
+	'''factor	: LEFT_PARENTHESIS expression RIGHT_PARENTHESIS'''
+	p[0] = ("d", p[2])
+
+def p_factor_float(p):
+	'''factor	: FLOAT_LITERAL'''
+	p[0] = ("FloatExpression", p[1])
+
+def p_factor_id(p):
+	'''factor	: ID'''
+	p[0] = ("ID", p[1])
 
 def p_error(p):
 	print('Syntax error at: %s' % p.value)
