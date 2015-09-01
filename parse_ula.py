2b5f75190ef2f71c83707d6f2836d23e129b7932
Muhummad Patel <muhummad.patel@gmail.com>
2015-09-02 00:49:38 +0200

    Construct and print out abstract syntax tree(AST).

--- parse_ula.py
+++ parse_ula.py
@@ -5,57 +5,65 @@ import sys
 lex_tokens = lex_ula.tokens
 tokens = [t for t in lex_ula.tokens if t not in lex_ula.non_code]
 
+def p_program_recursive(p):
+	'''program	: program statement'''
+	p[0] = p[1] + (p[2],)
+
 def p_program_terminal(p):
 	'''program	: statement'''
 	p[0] = ("Program", p[1])
 
-def p_program_recursive(p):
-	'''program	: program statement'''
-	p[0] = ("d", p[1], p[2])
-
 def p_statement_assignment(p):
 	'''statement	: ID EQUALS expression'''
-	p[0] = ("AssignStatement", p[1], p[3])
+	p[0] = ("AssignStatement", (("ID," + p[1],), p[3]))
 
 def p_expression_add(p):
 	'''expression	: expression AT term'''
-	p[0] = ("AddExpression", p[1], p[3])
+	p[0] = ("AddExpression", (p[1], p[3]))
 
 def p_expression_sub(p):
 	'''expression	: expression DOLLAR term'''
-	p[0] = ("SubExpression", p[1], p[3])
+	p[0] = ("SubExpression", (p[1], p[3]))
 
 def p_expression_term(p):
 	'''expression	: term'''
-	p[0] = ("d", p[1])
+	p[0] = p[1]
 
 def p_term_mul(p):
 	'''term	: term HASH factor'''
-	p[0] = ("MulExpression", p[1], p[2])
+	p[0] = ("MulExpression", (p[1], p[3]))
 
 def p_term_div(p):
 	'''term	: term AMPERSAND factor'''
-	p[0] = ("DivExpression", p[1], p[2])
+	p[0] = ("DivExpression", (p[1], p[3]))
 
 def p_term(p):
 	'''term	: factor'''
-	p[0] = ("d", p[1])
+	p[0] = p[1]
 
 def p_factor_brackets(p):
 	'''factor	: LEFT_PARENTHESIS expression RIGHT_PARENTHESIS'''
-	p[0] = ("d", p[2])
+	p[0] = p[2]
 
 def p_factor_float(p):
 	'''factor	: FLOAT_LITERAL'''
-	p[0] = ("FloatExpression", p[1])
+	p[0] = ("FloatExpression", ("FLOAT_LITERAL,"+p[1], ))
 
 def p_factor_id(p):
 	'''factor	: ID'''
-	p[0] = ("ID", p[1])
+	p[0] = ("IdentifierExpression", ("ID," + p[1],))
 
 def p_error(p):
 	print('Syntax error at: %s' % p.value)
 
+
+
+def print_tree(tree, tabs):
+	for subtree in tree:
+		if isinstance(subtree, str):
+			print(tabs + subtree)
+		else:
+			print_tree(subtree, tabs + '   ')
 
 
 def main(in_file, out_file):
@@ -63,7 +71,8 @@ def main(in_file, out_file):
 
 	in_data = in_file.read()
 	out = yacc.parse(in_data)
-	print(out)
+	print(out , '\n\n')
+	print_tree(out, '')
 
 
 if __name__ == '__main__':
